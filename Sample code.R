c714722fcc096d8e376c9a876df43b06312a3276
ntran41 <ntran4@uw.edu>
2023-02-27 14:06:58 -0800

    created dashboard

--- Sample code.R
+++ Sample code.R
@@ -4,3 +4,4 @@ CPIForecastData <- read_excel("CPIForecast.xlsx",
                               skip = 1)
 CPIHistoricalData <- read_excel("historicalcpi.xlsx", 
                             skip = 1)
+
